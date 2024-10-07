01776b6ab1b692b566ae958ef74161990b45987a
bensonc27 <bensonc27@nycstudents.net>
2024-10-07 12:19:44 -0400

    Log Students

---
+++ index.js
@@ -0,0 +1,6 @@
+const students = [
+  { name: "Alice", Age: 25, Grade: 85 },
+  { name: "Alex", Age: 22, Grade: 90 },
+  { name: "Charlie", Age: 21, Grade: 78 },
+  { name: "Diana", Age: 23, Grade: 95 },
+];
